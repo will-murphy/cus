a4e5e08d7058247ae47ef2ba90ad2c3c822901aa
Will Tennien Murphy <will.t.murphy@gmail.com>
2016-11-22 09:05:34 -0500

    changes

--- cus.js
+++ cus.js
@@ -175,5 +175,6 @@ toMixin.shuffle = (items) => {
     });
 };
 
+
 _.mixin(toMixin);
 module.exports = _;
--- how-to-publish.sh
+++ how-to-publish.sh
@@ -1,8 +1,9 @@
 
 # make changes to cus.js
-# increment changes in package.json
 git m "changes"
 npm login
+npm version # or just increment version in package.json
+git push origin master
 npm publish
 rm -r /Users/wtm/Dropbox/Codin/node_modules/cus
 cd /Users/wtm/Dropbox/Codin
