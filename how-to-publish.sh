467889bc7733a9a606d982dac3430477b1752854
Will Tennien Murphy <will.t.murphy@gmail.com>
2016-11-22 09:07:13 -0500

    changes

--- how-to-publish.sh
+++ how-to-publish.sh
@@ -2,7 +2,7 @@
 # make changes to cus.js
 git m "changes"
 npm login
-npm version # or just increment version in package.json
+npm version patch # or just increment version in package.json
 git push origin master
 npm publish
 rm -r /Users/wtm/Dropbox/Codin/node_modules/cus
