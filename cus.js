40c6073359880823fcd9fea3d112fa81edb2a214
Will <will@COLINs-MacBook-Pro.local>
2017-01-11 12:24:12 -0500

    changes

--- cus.js
+++ cus.js
@@ -3,99 +3,99 @@ let _ = require('underscore');
 
 let toMixin = {
     isEmpty: function(thing) {
-      return thing.length === 0;
+        return thing.length === 0;
     },
     isntEmpty: function(thing) {
-      return thing.length !== 0;
+        return thing.length !== 0;
     },
     method: function(thing, name) {
-      return function() {
-        return thing[name].apply(thing, argument);
-      };
+        return function() {
+            return thing[name].apply(thing, argument);
+        };
     },
     protract: function() {
-      var name, names, obj;
-      obj = arguments[0], name = arguments[1], names = 3 <= arguments.length ? [].slice.call(arguments, 2) : [];
-      if (name === void 0) {
-        return obj;
-      } else if (obj === void 0) {
-        return void 0;
-      } else {
-        return _.protract.apply({}, [obj[name]].concat(names));
-      }
+        var name, names, obj;
+        obj = arguments[0], name = arguments[1], names = 3 <= arguments.length ? [].slice.call(arguments, 2) : [];
+        if (name === void 0) {
+            return obj;
+        } else if (obj === void 0) {
+            return void 0;
+        } else {
+            return _.protract.apply({}, [obj[name]].concat(names));
+        }
     },
     streamToString: function(stream, callback) {
-      var input;
-      input = void 0;
-      input = "";
-      stream.on("data", function(chunk) {
-        return input += chunk.toString();
-      });
-      return stream.on("end", function() {
-        return callback(input);
-      });
+        var input;
+        input = void 0;
+        input = "";
+        stream.on("data", function(chunk) {
+            return input += chunk.toString();
+        });
+        return stream.on("end", function() {
+            return callback(input);
+        });
     },
     stringRepeat: function(n, str) {
-      if (n < 1) {
-        return '';
-      } else {
-        return str + arguments.callee(n - 1, str);
-      }
+        if (n < 1) {
+            return '';
+        } else {
+            return str + arguments.callee(n - 1, str);
+        }
     },
     createCheckin: function(n, cb) {
-      return function() {
-        if (--n <= 0) {
-          return cb();
-        }
-      };
+        return function() {
+            if (--n <= 0) {
+                return cb();
+            }
+        };
     },
     numberToEnglish: function(n, item) {
-      if (item === void 0) {
-        return ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'][n - 1];
-      } else {
-        switch (n) {
-          case 0:
-            return "no " + item + "s";
-          case 1:
-            return "one " + item;
-          default:
-            return "" + (_.numberToEnglish(n)) + " " + item + "s";
+        if (item === void 0) {
+            return ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'][n - 1];
+        } else {
+            switch (n) {
+                case 0:
+                    return "no " + item + "s";
+                case 1:
+                    return "one " + item;
+                default:
+                    return "" + (_.numberToEnglish(n)) + " " + item + "s";
+            }
         }
-      }
     },
     englishList: function(xs) {
-      switch (xs.length) {
-        case 1:
-        case 0:
-          return xs.join('');
-        case 2:
-          return xs[0] + " and " + xs[1];
-        default:
-          return xs.slice(0, -1).join(', ') + ", and " + xs[xs.length - 1];
-      }
+        switch (xs.length) {
+            case 1:
+            case 0:
+                return xs.join('');
+            case 2:
+                return xs[0] + " and " + xs[1];
+            default:
+                return xs.slice(0, -1).join(', ') + ", and " + xs[xs.length - 1];
+        }
     },
     segment: function(xs, n, includeTail) {
-      var result;
-      if (includeTail == null) {
-        includeTail = false;
-      }
-      result = [];
-      while (n <= xs.length) {
-        result.push(xs.slice(0, n));
-        xs = xs.slice(n);
-      }
-      if (true === includeTail && 0 < xs.length) {
-        result.push(xs);
-      }
-      return result;
+        var result;
+        if (includeTail == null) {
+            includeTail = false;
+        }
+        result = [];
+        while (n <= xs.length) {
+            result.push(xs.slice(0, n));
+            xs = xs.slice(n);
+        }
+        if (true === includeTail && 0 < xs.length) {
+            result.push(xs);
+        }
+        return result;
     },
     repeat: function(n, item) {
-      var _results;
-      _results = [];
-      while (n--) {
-        _results.push(item);
-      }
-      return _results;
+        var _results;
+        _results = [];
+        while (n--) {
+            _results.push(item);
+        }
+        return _results;
     }
 };
 
@@ -179,6 +179,15 @@ toMixin.shuffle = (items) => {
     });
 };
 
+toMixin.segment = (items, n = 1) => {
+    let result = [];
+    items.forEach((item, i) => {
+        let a = Math.floor(i / n);
+        result[a] = result[a] || [];
+        result[a].push(item);
+    });
+    return result;
+};
 
 _.mixin(toMixin);
 module.exports = _;
